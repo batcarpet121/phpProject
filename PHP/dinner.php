0661befa5d118699ec82f4420319e36537cd17a0
batcarpet121 <aaron.polaske@gmail.com>
2024-02-29 23:12:42 -0700

    Fixed content area height and style sheet links

--- PHP/dinner.php
+++ PHP/dinner.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/CSS/default.css">
-    <link rel="stylesheet" href="/CSS/breakfastStyles.css">
+    <link rel="stylesheet" href="/CSS/dinnerStyles.css">
     <title>Aaron's Diner - Breakfast</title>
 </head>
 
